5a91ab0c71def58783762abed7fd18851f520997
xingyuan kang <myaploy@gmail.com>
2019-04-24 23:51:39 +0700

    Add new test class

---
+++ src/test/java/camt/se234/lab10/service/TestGradeService.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class TestGradeService {
+    @Test
+    public void testGetGrade(){
+        GradeServiceImpl gradeService = new GradeServiceImpl();
+        assertThat(gradeService.getGrade(100),is("A"));
+        assertThat(gradeService.getGrade(79),is("A"));
+        assertThat(gradeService.getGrade(78.9),is("B"));
+        assertThat(gradeService.getGrade(75),is("B"));
+        assertThat(gradeService.getGrade(74.4),is("C"));
+        assertThat(gradeService.getGrade(59.2),is("C"));
+        assertThat(gradeService.getGrade(59),is("D"));
+        assertThat(gradeService.getGrade(34),is("D"));
+        assertThat(gradeService.getGrade(33),is("F"));
+        assertThat(gradeService.getGrade(0),is("F"));
+
+    }
+
+}
